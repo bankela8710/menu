2f5cb885db0d6f0390cb7de3f9fbddb5927b34d8
Marko Petronijevic <markobank87@gmail.com git config --global user.name Bankela>
2021-04-26 12:02:32 +0200

    initialization project

---
+++ src/Categories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+
+const Categories = ({ categories, filterItems }) => {
+    return (
+        <div className="btn-container">
+            {/* manual approach */}
+            {/* <button className="filter-btn" onClick={() => filterItems('all')}>
+                all
+            </button>
+            <button className="filter-btn" onClick={() => filterItems('breakfast')}>
+                breakfast
+            </button> */}
+
+            {/* dynamic approach */}
+            {categories.map((category, index) => {
+                return (
+                    <button type="button" className="filter-btn" key={index} onClick={() => filterItems(category)}> 
+                        {category}
+                    </button>
+                )
+            })}
+        </div>
+    )
+
+}
+
+
+
+export default Categories;
